f1639003313959dce32667d128625744c88cfc9a
Sam Gunaratne <sgunaratne@pivotal.io>
2018-11-16 16:53:43 +0000

    reattach controllers
    Signed-off-by: Ed King <eking@pivotal.io>
    Co-authored-by: Ed King <eking@pivotal.io>

--- pkg/controller/add_broker.go
+++ pkg/controller/add_broker.go
@@ -16,7 +16,9 @@ limitations under the License.
 
 package controller
 
+import "github.com/pivotal-cf/ism/pkg/controller/broker"
+
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	// AddToManagerFuncs = append(AddToManagerFuncs, broker.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, broker.Add)
 }
--- pkg/controller/add_brokeredserviceinstance.go
+++ pkg/controller/add_brokeredserviceinstance.go
@@ -16,7 +16,9 @@ limitations under the License.
 
 package controller
 
+import "github.com/pivotal-cf/ism/pkg/controller/brokeredserviceinstance"
+
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	// AddToManagerFuncs = append(AddToManagerFuncs, brokeredserviceinstance.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, brokeredserviceinstance.Add)
 }
